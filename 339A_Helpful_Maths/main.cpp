30ea0304e9f5a15b887fa1db9ae92bf332b375a0
jeong <uhdang@gmail.com>
2018-04-27 15:48:23 +0200

    convert string to int + vector

--- 339A_Helpful_Maths/main.cpp
+++ 339A_Helpful_Maths/main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 int main() {
 
     std::string s;
     std::cin >> s;
+    std::vector<int> c;
 
     for (int i = 0; i < s.length(); i++) {
-        std::cout << s[i] << "\n";
+        int i_s = s[i] - '0';
+        if (i_s != -5) {
+            c.push_back(i_s);
+        }
     }
 
-    int i_s = std::stoi(s);
+    std::sort(c.begin(), c.end());
 
-    std::cout << i_s << "\n";
+    std::string word;
+    std::string ch;
+
+    for (int j = 0; j < c.size(); j++) {
+        ch = std::to_string(c[j]);
+        word += ch;
+        if (j < c.size()-1) {
+            word += '+';
+        }
+    }
+
+    std::cout << word << "\n";
 
 }
