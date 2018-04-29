a0973604f28e2e2bc09f9fc4837df21a272330b6
jeong <uhdang@gmail.com>
2018-04-30 00:04:29 +0200

    improving

--- 158B_Taxi/main.cpp
+++ 158B_Taxi/main.cpp
@@ -4,52 +4,22 @@ using namespace std;
 
 int main() {
 
-    int c = 0, n_one = 0, n_two = 0, n_three = 0, n_four = 0, n;
+    int c = 0, n;
+    int A[5] = {0, 0, 0, 0, 0}, r;
     cin >> n;
     for (int i = 0; i < n; i++) {
-        int r;
         cin >> r;
-        switch(r) {
-            case 1:
-                n_one++;
-                break;
-                
-            case 2:
-                n_two++;
-                break;
-
-            case 3:
-                n_three++;
-                break;
-
-            case 4:
-                n_four++;
-                break;
-
-            default:
-                cout << "invalid value" << "\n";
-        }
+        A[r]++;
     }
 
-    c += n_four;
+    c = A[4]+A[3]+(A[2]/2);
 
-     if (n_one < n_three) {
-        c += n_three;
-        c += n_two/2;
-        c += n_two%2;
-        cout << c << "\n";
-
-    } else if (n_one == n_three) {
-        c += n_three;
-        c += n_two/2;
-        c += n_two%2;
+    if (A[1] < A[3] || A[1] == A[3]) {
+        c += A[2]%2;
         cout << c << "\n";
 
     }  else {
-        c += n_three;
-        c += n_two/2;
-        
-        int sum = (n_two%2) * 2 + (n_one - n_three);
+        int sum = (A[2]%2) * 2 + (A[1] - A[3]);
         c += sum/4;
         if (sum%4 != 0) {
             c++;
