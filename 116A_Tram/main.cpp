9d4f369675365c8e8fb1c79ce7743e8c69e50f07
jeong <uhdang@gmail.com>
2018-04-30 16:19:34 +0200

    a bit of improvement

--- 116A_Tram/main.cpp
+++ 116A_Tram/main.cpp
@@ -8,7 +8,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        c = c - a + b;
+        c += b-a;
         
         if (c > min) {
             min = c;
