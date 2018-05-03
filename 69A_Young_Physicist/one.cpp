26746ae04c22bf3e186a97c48dcf7a2310e29b2c
jeong <uhdang@gmail.com>
2018-05-03 19:57:27 +0200

    zero is false in bitwise operator

---
+++ 69A_Young_Physicist/one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int n, a = 0, b = 0, c = 0;
+   cin >> n;
+   for (int i = 0; i < n; i++) {
+       int x, y, z;
+       cin >> x >> y >> z;
+       a += x;
+       b += y;
+       c += z;
+   }
+   cout << (a|b|c?"NO\n":"YES\n");
+}
