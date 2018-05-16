7a40272c661b2461bd65b10c9f957a195d4f8e7e
uhdang <uhdang@gmail.com>
2018-05-16 20:33:37 +0000

    wrong answer

--- 266B_Queue_at_the_School/one.cpp
+++ 266B_Queue_at_the_School/one.cpp
@@ -10,21 +10,18 @@ int main() {
     for (int j = 0; j < t; j++) {
 
         for (int i = 0; i < n; i++) {
-            if (s[i] == "B") {
+            if (s[i] == 'B') {
                 b = true;
             }
-            if (b == true && s[i] == "G") {
+            if (b == true && s[i] == 'G') {
                 g = true;
             }
             if (b == true && g == true) {
-                // delete last character
                 o.pop_back();
-                // append "GB"
                 o += "GB";
-                // change b and g back to false
-                b, g = false;
+                b = false;
+                g = false;
             } else {
-                cout << s[i] << "\n";
                 o += s[i];
             }
         }
