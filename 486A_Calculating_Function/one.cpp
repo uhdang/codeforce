ae943b45faf9f12dcc48e8f3eb3aa623894b6869
uhdang <uhdang@gmail.com>
2018-05-21 17:33:08 +0000

    simplified but still time exceeding

--- 486A_Calculating_Function/one.cpp
+++ 486A_Calculating_Function/one.cpp
@@ -3,15 +3,11 @@
 using namespace std;
 
 int main() {
-    int n, sum = 0, add;
+    int n, sum = 0, p_m = 1;
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        if (i%2 != 0) {
-            add = i * -1;
-        } else {
-            add = i;
-        }
-        sum += add;
+        p_m = p_m * -1;
+        sum += p_m * i;
     }
     cout << sum << "\n";
 }
