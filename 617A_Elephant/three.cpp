2991ce894c60daf0f47d50729bf226cb51b8d72f
uhdang <uhdang@gmail.com>
2018-05-22 18:02:49 +0000

    this reasoning sucks!

--- 617A_Elephant/three.cpp
+++ 617A_Elephant/three.cpp
@@ -5,6 +5,5 @@ using namespace std;
 int main() {
     int x;
     cin >> x;
-    cout << (x%5 == 0 ? x/5 : x/5+1) << "\n";
-
+    cout << (x+4)/5 << "\n";
 }
--- 617A_Elephant/two.cpp
+++ 617A_Elephant/two.cpp
@@ -6,4 +6,9 @@ int main() {
     int x;
     cin >> x;
     cout << (x+4)/5 << "\n";
+    
+    // REASONING
+    // We want to increase 1 short of value that gets divided by adding 4 (5 - 1)
+    // so that it will give what we want
+
 }
