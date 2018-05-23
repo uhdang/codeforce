8fa513636038256ee741bb531e3a3b04f0d2955d
uhdang <uhdang@gmail.com>
2018-05-23 18:02:52 +0000

    unrefined passed

--- 208A_Dubstep/one.cpp
+++ 208A_Dubstep/one.cpp
@@ -3,5 +3,23 @@
 using namespace std;
 
 int main() {
+    string x, o = "";
+    int dubstep_count = 2;
+    cin >> x;
+    while (x.length() > 0) {
+        if(x.substr(0,3) == "WUB") {
+            dubstep_count++;
+            x.erase(0,3);
+        } else {
+            o += x[0];
+            x.erase(0,1);
+            dubstep_count = 0;
+        }
+
+        if (dubstep_count == 1) {
+            o += " ";
+        }
+    }
+    cout << o << "\n";
 
 }
