c1b7ae4dccf3e39607bbde064a9c5eed221be12c
uhdang <uhdang@gmail.com>
2018-05-28 16:53:41 +0000

    wrong answer on test 5

--- 318A_Even_Odds/one.cpp
+++ 318A_Even_Odds/one.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
-#include <vector>
+#include <math.h>
 
 using namespace std;
 
 int main() {
-
-    // Use Math !
-    // 1. difference case for odd + even number n
-
-    unsigned long long int n, k, inc = 1;
-    vector<unsigned long long int>v;
+    double n, k, half, inc = 1;
     cin >> n >> k;
-    while (inc <= n) {
-        v.push_back(inc);
+    half = ceil(n/2.0);
+    bool count_odd = (k <= half ? true : false);
+    k = (k <= half ? k : k-ceil(n/2));
+
+    for (int i = 1; i < k; i++) {
+        if (count_odd == 0) {
+            inc++;
+            count_odd = 1;
+        }
         inc += 2;
     }
-    inc = 2;
-    while (inc <= n) {
-        v.push_back(inc);
-        inc += 2;
-    }
-    cout << v.at(k-1) << "\n";
+    cout << inc << "\n";
 }
+
+
+
+
+// Use Math !
+//
+// when n%2 == 0
+//
+// i.e. n = 10, k = 3
+// [1 3 5 7 9 2 4 5 8 10]
+// ceil(n/2) = 5
+// x <= ceil(n/2), odd set
+// x > ceil(n/2), even set
+//
+// if k <= ceil(n/2), count from the beginning,
+// if k > ceil(n/2), count from ceil(n/2)th position
+//
+//
+//
+//
+//
+// when n%2 != 0
+// i.e. n = 7, k = 7
+// [1 3 5 7 2 4 6]
+//
+// x <= ceil(n/2) odd set
+// x > ceil(n/2) even set
+//
+// if k <= ceil(n/2), count from the beginning,
+// if k > ceil(n/2), count from ceil(n/2)th position
+//
+//
+
