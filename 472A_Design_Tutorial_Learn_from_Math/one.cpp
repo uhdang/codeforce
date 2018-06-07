99c004c553e35d2b0277b0db4fc4d67d7fd30c44
uhdang <uhdang@gmail.com>
2018-06-07 23:13:28 +0000

    time limit exceeded on test 12

--- 472A_Design_Tutorial_Learn_from_Math/one.cpp
+++ 472A_Design_Tutorial_Learn_from_Math/one.cpp
@@ -17,8 +17,8 @@ int isPrime (int n) {
 int main() {
     int n;
     cin >> n;
-    for (int i = 3; i < n; i++) {
-        for (int j = n; j > 3; j--) {
+    for (int i = n/2; i < n; i++) {
+        for (int j = n/2; j > 3; j--) {
             if (!isPrime(i) && !isPrime(j) && i + j == n) {
                 cout << i << " " <<  j << "\n";
                 return 0;
