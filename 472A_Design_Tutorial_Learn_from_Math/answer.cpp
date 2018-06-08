3ff6c0a1f5fd1e369ac9fdab08560dd9c95354be
uhdang <uhdang@gmail.com>
2018-06-08 21:51:42 +0200

    added explanation

--- 472A_Design_Tutorial_Learn_from_Math/answer.cpp
+++ 472A_Design_Tutorial_Learn_from_Math/answer.cpp
@@ -17,6 +17,14 @@ int main() {
     // if x is an odd number, 3, y is 8 and only y is composite number.
     // assigning 3, 5, 7 would not work because they are prime number. So, smallest odd composite number would be... 9.
     // but if x is 9, then if n is 13, y would be 4. and 4 is composite number. 
+    //
+    // GOAL:
+    // By specifying specific x for even or odd number, we want to make y as "EVEN" number, 
+    // so that it would always be composite number 
+    // in case of even value of n, possible value of x would be 4, 6, 8. 8 being maximum value.
+    // in case of odd value of n, the only value does not exceed 12 is 9. 
+    // Other odd value that will afford y to be even number larger than 2 are none.
+    // 3, 5, 7, 11 are all not composite number
 
     if (n%2 == 0) {
         cout << 4 << " " << n - 4 << "\n";
