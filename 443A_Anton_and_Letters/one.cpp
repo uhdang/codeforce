4d1733e98a49a3e19c3ee6ce5da932f474232b4e
uhdang <uhdang@gmail.com>
2018-06-10 16:41:17 +0200

    lesson 1. whitespace is counted, lesson 2. when you make an array of chaaracter, if you don't fill it in with elements, length doesn't get counted

--- 443A_Anton_and_Letters/one.cpp
+++ 443A_Anton_and_Letters/one.cpp
@@ -2,19 +2,21 @@
 // asci a - 97, z - 122
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main() {
-    int a[26] = {0}; 
+    int a[26] = {0};
     int c = 0;
-    string s;
-    cin >> s;
-    for (int i=1; i < (int)s.length();  i+=2) {
-       a[s[i]-97]++;
-       if (a[s[i]-97] == 1) {
+    char b[1000]; 
+    cin >> b;
+    for (int i=1; i<(int)strlen(b); i+=2) {
+       a[b[i]-97]++;
+       if (a[b[i]-97] == 1) {
            c++;
        }
     }
     cout << c << "\n";
+
 }
