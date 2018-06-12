edeace628a0ff1f36fb035a16b75755a13c2f27a
uhdang <uhdang@gmail.com>
2018-06-12 18:04:48 +0200

    finally passed! - out of bound behavior for a[26] has been causing error

--- 443A_Anton_and_Letters_STAR/one.cpp
+++ 443A_Anton_and_Letters_STAR/one.cpp
@@ -1,6 +1,3 @@
-// num of alphabet : 26
-// asci a - 97, z - 122
-
 #include <iostream>
 #include <string.h>
 
@@ -13,14 +10,11 @@ int main() {
     cin.getline(b, sizeof(b));
 
     for (int i=0; i<(int)strlen(b); i++) {
-        //cout << i << " : " << b[i]-97 << "\n";
-        //cout << "before: " << a[b[i]-97] << "\n";
-        //cout << "after: " << a[b[i]-97] << "\n";
 
-        if (a[b[i]-97] == 0) {
+
+        if (b[i]-97 >= 0 && b[i]-97 < 26 && a[b[i]-97] == 0) {
             a[b[i]-97]++;
             c++;
-        //cout << c << "\n";
         }
     }
     cout << c << "\n";
