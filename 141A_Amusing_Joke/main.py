b2a72b95fa60a5e8eab1b75d7836b459a0612955
jeong <uhdang@gmail.com>
2019-07-11 18:14:10 +0200

    141A

---
+++ 141A_Amusing_Joke/main.py
@@ -0,0 +1,10 @@
+# print("YES" if sorted(list(input()) + list(input())) == sorted(list(input())) else "NO")
+
+# s,i=sorted,input;print('YNEOS'[s(i()+i())!=s(i())::2])
+
+# s, i = sorted, input
+# foo = 'YNEOS'
+# print('YNEOS'[True::2])
+# print('YNEOS'[False::2])
+
+
