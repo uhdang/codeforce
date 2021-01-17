2478447d5c5bcd4068d5e42c20b1bd790afaaf61
Jung Woo James Jeong <uhdang@gmail.com>
2021-01-17 12:19:46 +0100

    1335A py

---
+++ 1335A_Candies_and_Two_Sisters_py/main.py
@@ -0,0 +1,10 @@
+# Attempt 1 - 124ms / 0KB
+for i in range(int(input())):
+    num_c = int(input())
+
+    # divide it
+    start = (num_c // 2) + 1
+
+    output = num_c - start
+    print(output)
+
