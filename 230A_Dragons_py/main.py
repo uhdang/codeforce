15f6b8b9aa8fed1764db2773e0fed3d6d0f11419
Jung Woo James Jeong <uhdang@gmail.com>
2021-01-27 00:06:14 +0100

    useful syntax

--- 230A_Dragons_py/main.py
+++ 230A_Dragons_py/main.py
@@ -1,29 +1,44 @@
-# 3rd Attempt - 154ms 300KB
+# # 3rd Attempt - 154ms 300KB
+#
+# # s = kilo's strength
+# # n = n_dragon
+# # x = dragon's strengh
+# # y = bonus
+# from operator import itemgetter
+#
+# s, n = map(int, input().split())
+#
+# mat = []
+# for i in range(n):
+#     x, y = map(int, input().split())
+#     mat.append([x, y])
+#
+# mat = sorted(mat, key=itemgetter(0))
+#
+# # print(mat)
+#
+# for i in mat:
+#     nxt_level = "NO"
+#     if s > i[0]:
+#         s += i[1]
+#         nxt_level = "YES"
+#     else:
+#         break
+#
+# print(nxt_level)
+#
 
-# s = kilo's strength
-# n = n_dragon
-# x = dragon's strengh
-# y = bonus
-from operator import itemgetter
-
+# by jackchess 62ms / 300KB
 s, n = map(int, input().split())
-
-mat = []
+a = []
 for i in range(n):
-    x, y = map(int, input().split())
-    mat.append([x, y])
+    a.append(list(map(int, input().split())))
+a.sort(key = (lambda x: x[0]))
 
-mat = sorted(mat, key=itemgetter(0))
-
-# print(mat)
-
-for i in mat:
-    nxt_level = "NO"
-    if s > i[0]:
-        s += i[1]
-        nxt_level = "YES"
-    else:
+for i in range(n):
+    if s <= a[i][0]:
+        print("NO")
         break
-
-print(nxt_level)
-
+    s += a[i][1]
+else:
+    print("YES")
