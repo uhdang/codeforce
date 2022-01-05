6044217ccd815cf705fcd466ca84ef6a37e0cf28
Jung Woo James Jeong <uhdang@gmail.com>
2022-01-06 00:13:07 +0100

    WIP - 1399A 6 attempts but not yet solved

---
+++ 1399A_Remove_Smallest/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+
+int main() {
+    int numTests, arrayLength, elem;
+    std::vector<int> a;
+    std::vector<std::string> outputArray;
+    std::cin >> numTests;
+
+    std::string output;
+    for (int i = 0; i < numTests; i++) {
+        a.clear();
+        output = "YES";
+        std::cin >> arrayLength;
+
+        for (int j = 0; j < arrayLength; j++) {
+            std::cin >>  elem;
+            a.push_back(elem);
+        }
+
+
+        for (int k = 0; k < arrayLength; k++) {
+            for (int l = 0; l < arrayLength; l++) {
+                int first = a.at(k);
+                int second = a.at(l);
+                if (abs(first - second) > 1) {
+                    output = "NO";
+                }
+            }
+        }
+
+        std::cout << output << "\n";
+
+//        outputArray.push_back(output);
+
+
+
+//        outputArray[i] = output;
+    }
+
+//    for (int i = 0; i < outputArray.size(); i++) {
+//        std::cout << outputArray.at(i);
+//    }
+}
